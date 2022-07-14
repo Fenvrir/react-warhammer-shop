277639c8ee32724acdbce0698d077e442855045b
Fenvrir <dragonofhokage@gmail.com>
2022-07-14 13:16:25 +0700

    fix cart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
         </Link>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>2310 ₽</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
